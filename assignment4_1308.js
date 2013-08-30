8630d56f708365e4e47850457080e127d91e0ea5
bnlevan <bnlevan@fullsail.edu>
2013-08-29 20:36:29 -0400

    added validate URL.

--- assignment4_1308.js
+++ assignment4_1308.js
@@ -12,28 +12,31 @@ var changeString = parseInt(numString);
    if (!isNaN(changeString)) {
    return true;
    } else {
-   return isNaN
-   }
+   return false
+   };
 
-}
+   };
+;
 
 //does a string follow a aaa@bbb.ccc pattern like an email address?
 function validateEmail(email){
    var index_at= email.indexOf('@')
    if( index_at === -1){
        return false;
-   }   
+   } ;  
 
     var name= email.substring(0,index_at);
     var domain=email.substring(index_at+1);
     return domain.indexOf('.') >0;
 
-    }
+    };
+;    
     
+//Does this string follow a phone number pattern
 function isAPhoneNumber(entry) {
 
   if (entry) {
-    //parse the value into small subsections
+    
     var openParen = entry.substring(0,1);
     var areaCode = entry.substring(1,4);
     var closeParen = entry.substring(4,5);
@@ -53,10 +56,10 @@ function isAPhoneNumber(entry) {
         return true
       } else {
          return false;
-      }
-      }
-   }
-   
+      };
+      };
+   };
+;   
 
 var arrayNum = function (getArray) {
 var addTotal = 0;
@@ -69,7 +72,8 @@ var addTotal = 0;
       }
       }
       return addTotal;
-   } // End of Number Function
+   } 
+;   
 
 function titleCase(objField) 
         {
@@ -79,22 +83,45 @@ function titleCase(objField)
                 outText = outText + objValues[i].substr(0, 1).toUpperCase() + objValues[i].substr(1).toLowerCase() + ((i < objValues.length - 1) ? " " : "");
             }
             return outText;
-        }           
+        };
+;        
+        
+var validateUrl = function (url) {
+   var stdProtocol = url.substring(0,7);
+   var secProtocol = url.substring(0,8);
+   var nonSecured = "http://";
+   var secured = "https://";
+
+   if (stdProtocol === nonSecured) {
+
+   return true;
+   } else {
+      return false
+      if (secProtocol === secured) {
+      return true;
+      } else {
+      return false;
+      };
+      };
+   };       
+;
 
 return {"checkNumeric": checkNumeric,
         "validateEmail": validateEmail,
         "isAPhoneNumber":isAPhoneNumber,
         "arrayNum": arrayNum,
-        "titleCase":titleCase
-}
-
+        "titleCase":titleCase,
+        "validateUrl":validateUrl
+      };
+;
 
 }//end myLibrary
 
 var newLib = myLibrary();
 
-console.log("Is this a number?" + newLib.checkNumeric("17"))
-console.log("Is this an email address?" + newLib.validateEmail("bnlevan@fullsail.edu"))
-console.log("is this a phone number?" + newLib.isAPhoneNumber("123-456-7890"))
-console.log ("Add the numbers in this array: " + newLib.arrayNum(["soccer", 7, "photography", 17, "bnlevan@fullsail.edu"]));
-console.log ("Was this strig title cased?" + newLib.titleCase("welcome to full sail university!"))
+console.log("Is this a number?" + newLib.checkNumeric("17"));
+console.log("Is this an email address?" + newLib.validateEmail("bnlevan@fullsail.edu"));
+console.log("is this a phone number?" + newLib.isAPhoneNumber("123-456-7890"));
+console.log("Add the numbers in this array: " + newLib.arrayNum(["soccer", 7, "photography", 17, "bnlevan@fullsail.edu"]));
+console.log("Was this string title cased? " + newLib.titleCase("welcome to full sail university!"));
+console.log("Is this a valid URL? " + newLib.validateUrl("http://www.google.com"));
